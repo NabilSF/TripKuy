0dd1f43a2ddd0a662fb19e6e0b6cdc10941d085a
maroisa <ahmadroisul412@gmail.com>
2025-12-29 11:00:23 +0700

    add index redirect

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IndexController extends AbstractController
+{
+    #[Route("/", name: "app_index")]
+    public function index(): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute("app_dashboard");
+        } else {
+            return $this->redirectToRoute("app_login");
+        }
+        return $this->render("index/index.html.twig", [
+            "controller_name" => "IndexController",
+        ]);
+    }
+}
